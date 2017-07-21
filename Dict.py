9db6bd977848dc06e5e6308cec48faa435c7218c
dingyalin <278173254@qq.com>
2017-07-21 21:23:19 +0800

    11

--- Dict.py
+++ Dict.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 
+'''
+'''
+
 
 class Dict(dict):
-    
+    '''
+    '''
+
     def __init__(self, names=(), values=(), **kw):
         super(Dict, self).__init__(**kw)
         for k, v in zip(names, values):
             self[k] = v
-            
+
     def __getattr__(self, key):
         try:
             value = self[key]
-            return value if not isinstance(value, dict) else  Dict(**value)
+            return value if not isinstance(value, dict) else Dict(**value)
         except KeyError:
             raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
 
     def __setattr__(self, key, value):
         self[key] = value
-        
-        
-        
+
+
 if __name__ == "__main__":
     D1 = Dict(**{"aa": {"bb": "11"}})
-    
+
     print D1.aa.bb
