a6f6d4e7d08c0e89a9064661e963eb3a6d918c2f
dingyalin <278173254@qq.com>
2017-08-10 18:58:32 +0800

    11

--- encrypt_key.py
+++ encrypt_key.py
@@ -21,8 +21,8 @@ def decrypt_key(key):
 
 
 if __name__ == "__main__":
-    # key = "83681b0ee23cabdd7c1c5d93dc7482e90608d542fef16435c79f3a155493ca62"
+    key = "eaff35d8c21a0448ff743b235830416231b2329bd248d1b7d27a5d99847f7bde980de97242a37597fc59312bbe115cb1e18bedb5f9a471813a1719fda3cd3bbe"
 
-    key = encrypt_key("OSUsermat_n&i")
+    # key = encrypt_key("OSUsermat_n%i")
     print key
     print decrypt_key(key)
